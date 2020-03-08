115395666af4f7a65c9711a7bf4841ea5db58f6a
feua <ferraresi.federico@outlook.com>
2020-03-08 17:18:46 +0000

    edit

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class Storyteller(MycroftSkill):
     @intent_file_handler('storyteller.intent')
     def handle_storyteller(self, message):
         self.speak_dialog('storyteller')
-
+	#provaprovaprova
 
 def create_skill():
     return Storyteller()
